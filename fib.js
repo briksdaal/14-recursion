d3120e1c26b5a064e4c8ffdd71badc584bf4d39c
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-04-10 02:31:59 +0300

    Implement fibs iterative version

---
+++ fib.js
@@ -0,0 +1,18 @@
+// n = 8, arr = [0, 1, 1, 2, 3, 5, 8, 13]
+
+function fibs (n) {
+    if (n === 0)
+        return [];
+    if (n === 1)
+        return [0];
+
+    let arr = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        arr = arr.concat(arr[i - 1] + arr[i - 2]);
+    }
+
+    return arr;
+}
+
+console.log(fibs(8))
